3cd4ad9053ba1e41771c95ed8233e7e7240390e2
Peter P. Marks <peter.marks@onepeloton.com>
2019-12-03 11:11:38 -0500

    Day 3 Part 2

--- day3/manhattan.py
+++ day3/manhattan.py
@@ -59,7 +59,9 @@ def get_intersections(plot1, plot2):
     intersections = set(plot1).intersection(plot2)
     results = []
     for (x, y) in intersections:
-        results.append( (x, y, abs(x) + abs(y)) )
+        distance = abs(x) + abs(y)
+        travel = plot1.index((x, y)) + plot2.index((x, y)) + 2
+        results.append( (x, y, distance, travel) )
     return results
 
 
@@ -72,13 +74,17 @@ def dont_stop_believin():
     gretel_crumbs = plot_wire(gretel)
     matches = get_intersections(hansel_crumbs, gretel_crumbs)
 
-    results = sorted(matches, key=itemgetter(2))
-    answer = results[0]
+    part1_results = sorted(matches, key=itemgetter(2))
+    part2_results = sorted(matches, key=itemgetter(3))
+    part1_answer = part1_results[0]
+    part2_answer = part2_results[0]
 
     print("Here's the results:")
-    print(f"There were a total of {len(results)} intersections.")
-    print(f"The closest intersection was at the coordinates {answer[0]}, {answer[1]}.")
-    print(f"The answer: The Manhattan distance was {answer[2]}!")
+    print(f"There were a total of {len(matches)} intersections.")
+    print(f"The closest intersection was at the coordinates {part1_answer[0]}, {part1_answer[1]}.")
+    print(f"Part 1 answer: The absolute Manhattan distance was {part1_answer[2]}!")
+    print(f"The least traveled intersection was at the coordinates {part2_answer[0]}, {part2_answer[1]}.")
+    print(f"Part 2 answer: The travelled Manhattan distance was {part2_answer[3]}!")
 
 
 if __name__ == "__main__":
@@ -118,5 +124,5 @@ def test_plot_wire():
 def test_get_intersections():
     plot1 = [ (1, 0), (2, 0), (3, 0), (4, 0), (4, -1), (4, -2), (4, -3), (3, -3), (2, -3), (2, -2) ]
     plot2 = [ (0, 1), (1, 1), (2, 1), (2, 0), (2, -1), (3, -1), (4, -1), (5, -1) ]
-    result = [ (2, 0, 2), (4, -1, 5) ]
+    result = [ (2, 0, 2, 6), (4, -1, 5, 12) ]
     assert get_intersections(plot1, plot2) == result
