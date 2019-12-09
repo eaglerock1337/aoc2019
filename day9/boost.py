dc69db5aef8b8a0bdb590b4b8fa8d5619aa85e06
Peter Marks <peter.marks@gmail.com>
2019-12-09 02:04:25 -0500

    Day 9 Part 2

---
+++ day9/boost.py
@@ -0,0 +1,41 @@
+from intcode import IntCode
+
+import os
+
+BOOST_FILE = "boost.txt"
+
+
+def read_opcode(filename):
+    """
+    Read in the opcode from a given filename and return as a list.
+    Currently only reads in a single opcode from the file.
+    """
+    if os.path.basename(os.getcwd()) == "aoc2019":
+        filename = os.path.join("day9", filename)
+
+    with open(filename, "r") as opcode_file:
+        opcode = opcode_file.readline()
+
+    string_opcode = opcode.split(",")
+    return [int(i) for i in string_opcode]
+
+
+def thank_you_captain(filename, instruction):
+    """
+    Captain Riggs! Captain Murtaugh! Thank you Captain! Thank you Captain!
+    Thank YOU Captain! Thank YOU Captain! Oh Captain, My Captain!
+    Enough with the Captain shit! Now get out of here!
+    """
+    boost = read_opcode(filename)
+    computer = IntCode(boost, [instruction])
+
+    output = computer.run()
+    keycode = output.pop()
+
+    print(f"BOOST Keycode: {keycode}")
+    return keycode
+
+
+if __name__ == "__main__":
+    thank_you_captain(BOOST_FILE, 1)
+    thank_you_captain(BOOST_FILE, 2)
---
+++ day9/test_boost.py
@@ -0,0 +1,22 @@
+import mock
+
+from day9.intcode import IntCode
+from day9.boost import (
+    read_opcode,
+    thank_you_captain,
+)
+
+# Tests
+
+
+def test_read_opcode():
+    string = "test.txt"
+    opcode = [1101, 10, 11, 11, 102, 2, 11, 12, 4, 12, 99, 0, 0]
+    result = read_opcode(string)
+    assert opcode == result
+
+
+def test_thank_you_captain():
+    filename = "test.txt"
+    result = thank_you_captain(filename)
+    assert result == 42
